e4471a471f299f5bbbe180ba2861bb89f141fdef
Richard Taylor <rtaylor@rightincode.com>
2018-06-19 23:44:00 -0400

    Adding non-deterministic example

---
+++ NonDeterministic/NonDeterministic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace NonDeterministic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPriceCalculator = new PriceCalculator();
+
+            Debug.WriteLine("The base price is $10 and the final price is $" 
+                + myPriceCalculator.CalculateFinalPrice(10).ToString());
+        }
+    }
+}
