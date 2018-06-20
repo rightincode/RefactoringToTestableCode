a608563f3a5f15c90b8b4dd1dea9baad0bab1f30
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-06-20 09:09:48 -0400

    Adding refactured NonDeterministic solution

---
+++ 1 - NonDeterministic-RF/NonDeterministic/PriceCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NonDeterministic
+{
+    public class PriceCalculator
+    {
+        public decimal CalculateFinalPrice(decimal basePrice, DateTime currentTime)
+        {
+            decimal finalPrice = 0;
+
+            if (currentTime.Hour < 10)
+            {
+                finalPrice = basePrice - (basePrice * decimal.Parse(".2"));
+            }
+            else if ((currentTime.Hour >= 10) && (currentTime.Hour < 12))
+            {
+                finalPrice = basePrice - (basePrice * decimal.Parse(".1"));
+            }
+            else
+            {
+                finalPrice = basePrice;
+            }
+
+            //Same logic as above but using a conditional operator
+            //finalPrice = currentTime.Hour < 10
+            //    ? basePrice - (basePrice * decimal.Parse(".2"))
+            //    : (currentTime.Hour >= 10) && (currentTime.Hour < 12) ? basePrice - (basePrice * decimal.Parse(".1")) 
+            //    : basePrice;
+
+            return finalPrice;
+        }
+    }
+}
---
+++ 1 - NonDeterministic-RF/NonDeterministic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NonDeterministic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPriceCalculator = new PriceCalculator();
+
+            Debug.WriteLine("The base price is $10 and the final price is $"
+                + myPriceCalculator.CalculateFinalPrice(10, DateTime.Now).ToString());
+        }
+    }
+}
